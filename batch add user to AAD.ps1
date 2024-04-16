3fa2cd538c1bd4f2e7f4b2ec5798895175ff1ed6
confusedstardust <74608709+confusedstardust@users.noreply.github.com>
2024-04-16 13:37:30 +0800

    Update batch add user to AAD.ps1

--- batch add user to AAD.ps1
+++ batch add user to AAD.ps1
@@ -2,7 +2,7 @@ param(
     [Parameter(Mandatory = $true, HelpMessage = "Enter TenantId")][String]$TenantId,
     [Parameter(Mandatory = $true, HelpMessage = "Enter Group's ObjectId")][String]$GroupId,
     [Parameter(Mandatory = $true, HelpMessage = "Enter User List Path")][String]$FilePath,
-    [Parameter(Mandatory = $true, HelpMessage = "Enter one of the following options: ObjectID, Email, MailNickname")]
+    [Parameter(Mandatory = $true, HelpMessage = "Enter one of the following options: ObjectID, Email, useMailNickname")]
     [ValidateSet("ObjectID", "Email", "MailNickname")]
     [String]$Option
 )
@@ -14,15 +14,16 @@ $userList = Get-Content -Path $FilePath
 
 Write-Host $Option
 #batch add to group 
-if($Option -eq'ObjectID'){
+if ($Option -eq 'ObjectID') {
     foreach ($user in $userList) {
-    Add-AzureADGroupMember -ObjectId $GroupId -RefObjectId $user
-    Write-Host "the user: $user has been added"
+        Add-AzureADGroupMember -ObjectId $GroupId -RefObjectId $user
+        Write-Host "the user $user has been added"
     }
-}else{
-  foreach ($user in $userList) {
-    $userObjectId = (Get-AzureADUser -Filter "$Option eq '$user'").ObjectId
-    Add-AzureADGroupMember -ObjectId $GroupId -RefObjectId $userObjectId
-    Write-Host "the user: $user has been added"
+}
+else {
+    foreach ($user in $userList) {
+        $userObjectId = (Get-AzureADUser -Filter "$Option eq '$user'").ObjectId
+        Add-AzureADGroupMember -ObjectId $GroupId -RefObjectId $userObjectId
+        Write-Host "the user $user has been added"
     }
 }
