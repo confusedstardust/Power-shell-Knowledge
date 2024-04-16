645a82182580bc22342c2b3ad92f1b388f0a4d28
confusedstardust <74608709+confusedstardust@users.noreply.github.com>
2024-04-16 13:35:05 +0800

    Create batch add user to AAD.ps1

---
+++ batch add user to AAD.ps1
@@ -0,0 +1,28 @@
+param(
+    [Parameter(Mandatory = $true, HelpMessage = "Enter TenantId")][String]$TenantId,
+    [Parameter(Mandatory = $true, HelpMessage = "Enter Group's ObjectId")][String]$GroupId,
+    [Parameter(Mandatory = $true, HelpMessage = "Enter User List Path")][String]$FilePath,
+    [Parameter(Mandatory = $true, HelpMessage = "Enter one of the following options: ObjectID, Email, MailNickname")]
+    [ValidateSet("ObjectID", "Email", "MailNickname")]
+    [String]$Option
+)
+#connect to AzureAD
+Connect-AzureAD -TenantId $TenantId
+
+#get user list 
+$userList = Get-Content -Path $FilePath
+
+Write-Host $Option
+#batch add to group 
+if($Option -eq'ObjectID'){
+    foreach ($user in $userList) {
+    Add-AzureADGroupMember -ObjectId $GroupId -RefObjectId $user
+    Write-Host "the user: $user has been added"
+    }
+}else{
+  foreach ($user in $userList) {
+    $userObjectId = (Get-AzureADUser -Filter "$Option eq '$user'").ObjectId
+    Add-AzureADGroupMember -ObjectId $GroupId -RefObjectId $userObjectId
+    Write-Host "the user: $user has been added"
+    }
+}
